85a7817882d1d7be6320e9fa01487e40a5cd6890
Afei <aboynamedafei@126.com>
2023-04-17 14:06:51 +0800

    init project

---
+++ src/main/java/vip/afei/redisson/distributed/executorservice/RedissonDistributedExecutorServiceApplication.java
@@ -0,0 +1,13 @@
+package vip.afei.redisson.distributed.executorservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RedissonDistributedExecutorServiceApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RedissonDistributedExecutorServiceApplication.class, args);
+    }
+
+}
